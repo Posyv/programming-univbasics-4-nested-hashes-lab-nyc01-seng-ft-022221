3c698edcc91c19d4730c353e29132241b2037239
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 19:09:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,6 +55,7 @@ def dennis_ritchies_language
         :known_for => "Unix",
         :languages => ["C"]
       }
+			
     }
 
 end
@@ -67,7 +68,8 @@ def adding_matz
 # }
 # return the entire updated hash
 
-	programmer_hash =
+	programmer_hash = programmer_hash[:yukihiro_matsumoto] = {:known_for=>"Ruby",:languages=>["LISP", "C"]}
+programmer_hash
  		{
       :grace_hopper => {
         :known_for => "COBOL",
